63260b55763da1ddbb354a241cc7bad72d5acaf2
Kim Sha <KSha@Kims-MacBook-Pro.local>
2020-10-30 14:42:48 -0400

    Add basic styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,8 @@ export default function Home() {
   return (
     <div style={{ color: `purple` }}>
       <Link to="/contact/">Contact</Link>
+      <br/>
+      <Link to="/about-css-modules/">CSS Modules</Link>
       <Header headerText="Hello Gatsby!" />
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
