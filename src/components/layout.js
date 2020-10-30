a2f5c26270858d182ff6e9bef2e3724b8cd05e98
Kim Sha <KSha@Kims-MacBook-Pro.local>
2020-10-30 18:03:01 -0400

    Add graphQL plugins

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,15 @@
 import React from "react"
-import { Link } from "gatsby"
+import { useStaticQuery, Link, graphql } from "gatsby"
+import { css } from "@emotion/core"
+import { rhythm } from "../utils/typography"
+
 const ListLink = props => (
   <li style={{ display: `inline-block`, marginRight: `1rem` }}>
     <Link to={props.to}>{props.children}</Link>
   </li>
 )
 
-export default function Layout({ children }) {
+export function Layout({ children }) {
   return (
     <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
       <header style={{ marginBottom: `1.5rem` }}>
@@ -18,8 +21,54 @@ export default function Layout({ children }) {
           <ListLink to="/about/">About</ListLink>
           <ListLink to="/contact/">Contact</ListLink>
           <ListLink to="/about-css-modules/">CSS</ListLink>
+          <ListLink to="/my-files/">Files</ListLink>
         </ul>
       </header>
+      {children}
+    </div>
+  )
+}
+
+export function PandaLayout({ children }) {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  )
+  return (
+    <div
+      css={css`
+        margin: 0 auto;
+        max-width: 700px;
+        padding: ${rhythm(2)};
+        padding-top: ${rhythm(1.5)};
+      `}
+    >
+      <Link to={`/`}>
+        <h3
+          css={css`
+            margin-bottom: ${rhythm(2)};
+            display: inline-block;
+            font-style: normal;
+          `}
+        >
+          {data.site.siteMetadata.title}
+        </h3>
+      </Link>
+      <Link
+        to={`/about/`}
+        css={css`
+          float: right;
+        `}
+      >
+        About
+      </Link>
       {children}
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,13 @@
 import React from "react"
 import Header from "../components/header"
-import Layout from "../components/layout"
+import { graphql } from "gatsby"
+import { css } from "@emotion/core"
+import {Layout, PandaLayout} from "../components/layout"
+import { rhythm } from "../utils/typography"
 
-export default function Home() {
+
+export default function Home({ data }) {
+  console.log(data)
   return (
     <Layout>
       <Header headerText="Hello Gatsby!" />
@@ -13,6 +18,65 @@ export default function Home() {
         What do I like to do? Lots of course but definitely enjoy building
         websites.
       </p>
+      <PandaLayout>
+        <div>
+          <h1
+            css={css`
+              display: inline-block;
+              border-bottom: 1px solid;
+            `}
+          >
+            Amazing Pandas Eating Things
+          </h1>
+          <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+          {data.allMarkdownRemark.edges.map(({ node }) => (
+            <div key={node.id}>
+              <h3
+                css={css`
+                  margin-bottom: ${rhythm(1 / 4)};
+                `}
+              >
+                {node.frontmatter.title}{" "}
+                <span
+                  css={css`
+                    color: #bbb;
+                  `}
+                >
+                  — {node.frontmatter.date}
+                </span>
+              </h3>
+              <p>{node.excerpt}</p>
+            </div>
+          ))}
+        </div>
+      </PandaLayout>
+      <PandaLayout>
+        <h1>Amazing Pandas Eating Things</h1>
+        <div>
+          <img
+            src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
+            alt="Group of pandas eating bamboo"
+          />
+        </div>
+      </PandaLayout>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
