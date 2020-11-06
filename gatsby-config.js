e9ca625e7702dc7e50fb2593694769586c4f0d6b
Kim Sha <KSha@Kims-MacBook-Pro.local>
2020-11-05 21:10:34 -0500

    reset initial content

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,23 @@ module.exports = {
         path: `${__dirname}/content/`,
       },
     },
-    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          `gatsby-remark-responsive-iframe`,
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+            },
+          },
+        ],
+      },
+    },
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
