d0b186991cdad4380e2f95da7cb9e51c86132a96
Kim Sha <40638927+Kim-Sha@users.noreply.github.com>
2020-11-07 18:14:45 -0500

    Search bar (#27)
    * add blog tags
    
    * implement search bar
    
    Co-authored-by: Kim Sha <KSha@Kims-MacBook-Pro.local>

--- src/components/nav-links/nav-links.js
+++ src/components/nav-links/nav-links.js
@@ -6,19 +6,19 @@ const NavLinks = () => {
     return (
         <div className="flex lg:flex-col">
             <div className="lg:mb-3 ml-2">
-                <Link to="/">
+                <Link to="/" className="hover:opacity-75">
                     <FaHome className="inline-block h-8 w-8" />
                     <span className="hidden lg:inline-block pl-2">Home</span>
                 </Link>
             </div> 
             <div className="lg:mb-3 ml-2">
-                <Link to="/writing">
+                <Link to="/writing" className="hover:opacity-75">
                     <FaPenNib className="inline-block h-8 w-8" />
                     <span className="hidden lg:inline-block pl-3">Writing</span>
                 </Link>
             </div>
             <div className="lg:mb-3 ml-2">
-                <Link to="/contact-form">
+                <Link to="/contact-form" className="hover:opacity-75">
                     <FaEnvelope className="inline-block h-8 w-8" />
                     <span className="hidden lg:inline-block pl-3">Contact</span>
                 </Link>
--- src/pages/writing.js
+++ src/pages/writing.js
@@ -1,10 +1,46 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link, graphql } from "gatsby"
 import Layout from "../templates/layout"
+import ProjectTags from "../components/projects/project-tags"
+import { FaSearch } from "react-icons/fa"
 
 export default function Writing({ data }) {
 
   const { blogs } = data;
+  const emptyQuery = ""
+  const [state, setState] = useState({
+    filteredData: [],
+    query: emptyQuery,
+  })
+
+  const handleSearchInput = event => {
+    const query = event.target.value
+    const posts = blogs.edges || []
+     // return all filtered posts
+    const filteredData = posts.filter(post => {
+      // destructure data from post frontmatter
+      const { description, title, tags } = post.node.frontmatter
+      return (
+        // standardize data with .toLowerCase()
+        // return true if the description, title or tags contains the query string
+        description.toLowerCase().includes(query.toLowerCase()) ||
+        title.toLowerCase().includes(query.toLowerCase()) ||
+        (tags && tags
+          .join("") // convert tags from an array to string
+          .toLowerCase()
+          .includes(query.toLowerCase()))
+      )
+    })
+    // update state according to the latest query and results
+    setState({
+      query, // with current query string from the `Input` event
+      filteredData, // with filtered data from posts.filter(post => (//filteredData)) above
+    })
+  }
+
+  const { filteredData, query } = state
+  const hasSearchResults = filteredData && (query !== emptyQuery)
+  const posts = hasSearchResults ? filteredData : blogs.edges
 
   return (
     <Layout>
@@ -13,19 +49,37 @@ export default function Writing({ data }) {
         <header className="prose mb-6">
           <h1 className="prose">Writing</h1>
         </header>
-        
-        {blogs.edges.map(({ node }) => (
-          <Link to={node.fields.slug}>
-            <div className="prose mb-6" key={node.id}>
-              <h3>
-                {node.frontmatter.title}{" "}
-                <span className="italic text-base text-front">
-                  — {node.frontmatter.date}
-                </span>
-              </h3>
-              <p>{node.frontmatter.description ? node.frontmatter.description : node.excerpt}</p>
+
+        <div className="mb-6">
+          <div class="bg-back-secondary flex items-center rounded-md shadow w-full lg:w-3/5 h-10">
+            <input class="bg-back-secondary rounded-l-full w-full h-full py-4 px-6 leading-tight focus:outline-none"
+                   id="search" type="text" placeholder="Search" onChange={handleSearchInput}/>
+
+            <div class="p-4">
+              <button class="bg-primary text-back-secondary rounded-full p-2 hover:bg-opacity-75 focus:outline-none w-full h-full flex items-center justify-center">
+                <FaSearch />
+              </button>
             </div>
-          </Link>
+          </div>
+        </div>
+        
+        {posts.map(({ node }) => (
+          <div>
+            <Link to={node.fields.slug}>
+              <div className="prose" key={node.id}>
+                <h4>
+                  {node.frontmatter.title}{" "}
+                  <span className="italic text-sm text-front">
+                    — {node.frontmatter.date}
+                  </span>
+                </h4>
+                <p className="text-sm">{node.frontmatter.description ? node.frontmatter.description : node.excerpt}</p>
+              </div>
+            </Link>
+            <ul className="pr-2 mb-6 italic">
+              {node.frontmatter.tags && <ProjectTags tags={node.frontmatter.tags} />}
+            </ul>
+          </div>
           ))}
       </div>
     </Layout>
@@ -43,6 +97,7 @@ export const query = graphql`
             title
             date(formatString: "DD MMMM, YYYY")
             description
+            tags
           }
           fields {
             slug
