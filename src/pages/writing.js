f25c64a25cfb1cb056fce945c93d38181f6700b5
Kim Sha <KSha@Kims-MacBook-Pro.local>
2020-11-07 18:09:18 -0500

    implement search bar

--- src/pages/writing.js
+++ src/pages/writing.js
@@ -1,11 +1,46 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link, graphql } from "gatsby"
 import Layout from "../templates/layout"
 import ProjectTags from "../components/projects/project-tags"
+import { FaSearch } from "react-icons/fa"
 
 export default function Writing({ data }) {
 
   const { blogs } = data;
+  const emptyQuery = ""
+  const [state, setState] = useState({
+    filteredData: [],
+    query: emptyQuery,
+  })
+
+  const handleSearchInput = event => {
+    const query = event.target.value
+    const posts = blogs.edges || []
+     // return all filtered posts
+    const filteredData = posts.filter(post => {
+      // destructure data from post frontmatter
+      const { description, title, tags } = post.node.frontmatter
+      return (
+        // standardize data with .toLowerCase()
+        // return true if the description, title or tags contains the query string
+        description.toLowerCase().includes(query.toLowerCase()) ||
+        title.toLowerCase().includes(query.toLowerCase()) ||
+        (tags && tags
+          .join("") // convert tags from an array to string
+          .toLowerCase()
+          .includes(query.toLowerCase()))
+      )
+    })
+    // update state according to the latest query and results
+    setState({
+      query, // with current query string from the `Input` event
+      filteredData, // with filtered data from posts.filter(post => (//filteredData)) above
+    })
+  }
+
+  const { filteredData, query } = state
+  const hasSearchResults = filteredData && (query !== emptyQuery)
+  const posts = hasSearchResults ? filteredData : blogs.edges
 
   return (
     <Layout>
@@ -14,8 +49,21 @@ export default function Writing({ data }) {
         <header className="prose mb-6">
           <h1 className="prose">Writing</h1>
         </header>
+
+        <div className="mb-6">
+          <div class="bg-back-secondary flex items-center rounded-md shadow w-full lg:w-3/5 h-10">
+            <input class="bg-back-secondary rounded-l-full w-full h-full py-4 px-6 leading-tight focus:outline-none"
+                   id="search" type="text" placeholder="Search" onChange={handleSearchInput}/>
+
+            <div class="p-4">
+              <button class="bg-primary text-back-secondary rounded-full p-2 hover:bg-opacity-75 focus:outline-none w-full h-full flex items-center justify-center">
+                <FaSearch />
+              </button>
+            </div>
+          </div>
+        </div>
         
-        {blogs.edges.map(({ node }) => (
+        {posts.map(({ node }) => (
           <div>
             <Link to={node.fields.slug}>
               <div className="prose" key={node.id}>
