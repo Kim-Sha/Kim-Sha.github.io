a39f02e8fae174c5c0e7fcd4245451aa549d86e8
Kim Sha <KSha@Kims-MacBook-Pro.local>
2020-11-08 17:53:32 -0500

    Restyle nav bar on mobile

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import { FaEnvelope } from "react-icons/fa"
-import { HiMenuAlt3 } from "react-icons/hi"
+import { HiMenuAlt2 } from "react-icons/hi"
 import { ProfileType } from "../../types"
 import { Link } from "gatsby"
 
@@ -9,11 +9,14 @@ const Header = ({ initials }) => {
   const [toggle, setToggle] = useState(false)
 
   return (
-    <header className="bg-front lg:bg-back sticky lg:static top-0 z-40">
-      <div className="flex justify-between p-4 lg:px-8">
+    <header className="p-4 bg-none sticky lg:static top-0 z-40 space-y-0">
+      <div className={`${toggle ? 'rounded-b-none' : null} rounded-lg bg-front flex justify-between p-4 lg:px-8`}>
+        <button onClick={() => setToggle(!toggle)} onKeyDown={() => setToggle(!toggle)} className="lg:hidden focus:outline-none">
+          <HiMenuAlt2 className={`${toggle ? 'text-primary' : 'text-back'} w-8 h-8`}/>
+        </button>
         <div className="hover:text-primary">
           <Link to='/'>
-            <span className="fill-current inline-flex w-8 h-8 lg:w-14 lg:h-14 lg:mt-4 font-header font-bold text-xs lg:text-xl justify-center items-center text-center text-back lg:text-front border-2 border-solid border-back lg:border-front rounded-full">
+            <span className="inline-flex w-8 h-8 lg:w-14 lg:h-14 lg:mt-4 font-header font-bold text-xs lg:text-xl justify-center items-center text-center text-back lg:text-front border-2 border-solid border-back lg:border-front rounded-full">
               {initials}
             </span>
           </Link>
@@ -27,11 +30,8 @@ const Header = ({ initials }) => {
             <span className="hidden lg:block">Contact me</span>
           </Link>
         </div>
-        <button onClick={() => setToggle(!toggle)} onKeyDown={() => setToggle(!toggle)} className="lg:hidden focus:outline-none">
-          <HiMenuAlt3 className={`${toggle ? 'text-primary' : 'text-back'} w-8 h-8`}/>
-        </button>
       </div>
-      <div className={`${toggle ? 'block' : 'hidden'} px-2 pt-2 pb-4 lg:hidden`}>
+      <div className={`${toggle ? 'block' : 'hidden'} rounded-t-none px-2 pb-4 lg:hidden bg-front rounded-lg`}>
         <Link to='/' className="block px-2 py-1 text-back text-xs font-semibold rounded hover:bg-gray-900">Home</Link>
         <Link to='/writing' className="mt-1 block px-2 py-1 text-back text-xs font-semibold rounded hover:bg-gray-900">Writing</Link>
         <Link to= '/contact-form' className="mt-1 block px-2 py-1 text-back text-xs font-semibold rounded hover:bg-gray-900">Contact</Link>
