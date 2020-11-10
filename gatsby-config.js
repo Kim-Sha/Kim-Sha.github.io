5440548c1c3eab9a73eef61fe8fba4a2bdc92765
Kim Sha <KSha@Kims-MacBook-Pro.local>
2020-11-10 10:42:13 -0500

    place GA ID in head

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,14 +73,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
           process.env.GA_TRACKING_ID, // Google Analytics / GA
           // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
           // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
         ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+        },
       },
     },
     `gatsby-plugin-offline`,
