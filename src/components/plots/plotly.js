c84f27ba963444b2c22ecf508984ef2c0b58f369
Kim Sha <KSha@Kims-MacBook-Pro.local>
2021-02-16 01:11:25 -0500

    dark / light mode support for plotly integration

--- src/components/plots/plotly.js
+++ src/components/plots/plotly.js
@@ -1,46 +1,48 @@
-import React from 'react'
+import React, { useContext } from "react"
+import { ThemeContext } from "../../templates/theme-context"
 import Loadable from 'react-loadable'
 // import { FoldingSpinner } from './Spinners'
-// import { useDarkMode } from 'hooks'
 import { SiGatsby } from "react-icons/si"
 
 const Plotly = Loadable({
-  loader: () => import(`react-plotly.js`),
-  loading: ({ timedOut }) =>
+    loader: () => import(`react-plotly.js`),
+    loading: ({ timedOut }) =>
     timedOut ? (
-      <blockquote>Error: Loading Plotly timed out.</blockquote>
+        <blockquote>Error: Loading Plotly timed out.</blockquote>
     ) : (
     //   <FoldingSpinner />
         <SiGatsby className="w-6 h-6" />
     ),
-  timeout: 10000,
+    timeout: 10000,
 })
 
 export const LazyPlot = ({ layout, style, config, ...rest }) => {
-//   const [colorMode] = useDarkMode()
-  return (
+    
+    // Reference hook for 'dark' or 'light' theme
+    const { theme } = useContext(ThemeContext)
+
+    return (
     <Plotly
-      layout={{
+        layout={{
         margin: { t: 0, r: 0, b: 0, l: 0 },
         paper_bgcolor: `rgba(0, 0, 0, 0)`,
         plot_bgcolor: `rgba(0, 0, 0, 0)`,
         font: {
-        //   color: colorMode !== `light` ? `white` : `black`,
-          color: `white`,
-          size: 16,
+            color: theme === `light` ? `black` : `white`,
+            size: 16,
         },
         // The next 3 options make the plot responsive.
         autosize: true,
         ...layout,
-      }}
-      style={{ width: `100%`, ...style }}
-      useResizeHandler
-      config={{
+        }}
+        style={{ width: `100%`, ...style }}
+        useResizeHandler
+        config={{
         displayModeBar: false,
         showTips: false,
         ...config,
-      }}
-      {...rest}
+        }}
+        {...rest}
     />
-  )
+    )
 }
