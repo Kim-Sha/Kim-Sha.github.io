5be9e0b2aa80350dfab8d3972e161a780d377150
Kim Sha <KSha@Kims-MacBook-Pro.local>
2021-02-21 01:00:00 -0500

    clean plotly integration; loading spinner; serverless blog post

--- src/components/plots/plotly.js
+++ src/components/plots/plotly.js
@@ -1,8 +1,7 @@
 import React, { useContext } from "react"
 import { ThemeContext } from "../../templates/theme-context"
 import Loadable from 'react-loadable'
-// import { FoldingSpinner } from './Spinners'
-import { SiGatsby } from "react-icons/si"
+import '../../styles/loading-cube.css'
 
 const Plotly = Loadable({
     loader: () => import(`react-plotly.js`),
@@ -10,8 +9,17 @@ const Plotly = Loadable({
     timedOut ? (
         <blockquote>Error: Loading Plotly timed out.</blockquote>
     ) : (
-    //   <FoldingSpinner />
-        <SiGatsby className="w-6 h-6" />
+        <div class="sk-cube-grid">
+            <div class="sk-cube sk-cube1"></div>
+            <div class="sk-cube sk-cube2"></div>
+            <div class="sk-cube sk-cube3"></div>
+            <div class="sk-cube sk-cube4"></div>
+            <div class="sk-cube sk-cube5"></div>
+            <div class="sk-cube sk-cube6"></div>
+            <div class="sk-cube sk-cube7"></div>
+            <div class="sk-cube sk-cube8"></div>
+            <div class="sk-cube sk-cube9"></div>
+        </div>
     ),
     timeout: 10000,
 })
@@ -24,23 +32,24 @@ export const LazyPlot = ({ layout, style, config, ...rest }) => {
     return (
     <Plotly
         layout={{
-        margin: { t: 0, r: 0, b: 0, l: 0 },
-        paper_bgcolor: `rgba(0, 0, 0, 0)`,
-        plot_bgcolor: `rgba(0, 0, 0, 0)`,
-        font: {
-            color: theme === `light` ? `black` : `white`,
-            size: 16,
-        },
-        // The next 3 options make the plot responsive.
-        autosize: true,
-        ...layout,
+            margin: { t: 25, r: 25, b: 0, l: 0 },
+            // Transparent background
+            paper_bgcolor: `rgba(0, 0, 0, 0)`,
+            plot_bgcolor: `rgba(0, 0, 0, 0)`,
+            font: {
+                color: theme === `light` ? '#343d52' : '#edf2f7',
+                family: "Roboto, sans-serif",
+            },
+            // The next 3 options make the plot responsive.
+            autosize: true,
+            ...layout,
         }}
         style={{ width: `100%`, ...style }}
         useResizeHandler
         config={{
-        displayModeBar: false,
-        showTips: false,
-        ...config,
+            displayModeBar: false,
+            showTips: false,
+            ...config,
         }}
         {...rest}
     />
